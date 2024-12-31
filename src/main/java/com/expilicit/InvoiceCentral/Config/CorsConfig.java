c75c0a64b4cf4154b54c02d70855c1199cfc2d58
princedickson <princedickson03@gmail.com>
2024-12-31 02:25:14 +0100

    Cors configuration

---
+++ src/main/java/com/expilicit/InvoiceCentral/Config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.expilicit.InvoiceCentral.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*")
+                .maxAge(3600L)
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization")
+                .allowCredentials(true);
+    }
+}
